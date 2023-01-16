ffd5b95f2484340c40c7e00d69a01c9a0ff2d8f5
happyfruitnijna <happyfruitninja@gmail.com>
2023-01-16 10:58:13 -0500

    milestone3 : fetched and working on inserting API to DOM

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,63 +1,60 @@
-/* BRINING IN DATA FROM BACK-END -> this show the objects in console*/
+//BRINING IN DATA FROM BACK-END -> this shows the objects in console
+
 fetch("http://localhost:3000/api/products/")
   .then((data) => {
     return data.json();
   })
   .then((products) => {
-    console.log(products);
+     console.log(products);
   });
 
 console.log("hi");
+const productCards = document.getElementById("items");
+console.log(productCards);
 
 // ACCESSING PRODUCT INFO
-// get section and create product card that contains an article of heading, image, description
-// give each element of a product attributes - element.setAttribute('name', 'value')
+//TODO: wrap all of the code below into a function called "insertProducts(products)"
+function insertProducts(products) {
+  //TODO: declare productCards variable and set initially to empty string const (ex.productCards = "")
+  const productCard = ""; //KATE what is this and why do I need this?
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    console.log(product); //KATE console won't show populated the loop. it shows a flash but disappears
 
-const section = document.getElementById("items");
-const href = "./product.html?id=107fb5b75607497b96722bda5b504926";
+    //TODO: declare a variable that is going to be a string for the commented out html in index page // KATE I don't understand this, "a string"?
 
+    const productElement = document.createElement('article');
+    
+    //  NOTE: use template string - check out the link from Scott (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals)
+    //  NOTE: add expression${} for each part of html that changes from product to product - dot notation ex. product.name =
 
-const productCard = document.createElement("a");
-productCard.setAttribute(
-  "href",
-  "./product.html?_id="
-);
+    //TODO: append the current productsCards productCards =+ productCard
+    productCards.append(productCard);
+    productCard.append(article);
 
-const image = document.createElement("img");
-image.setAttribute("src", "../../back-end/images/kanap01.jpeg" );
+    //TODO: set innerHTML section element to productCards string (ex.section.innerHTML = productCards)
 
+    productCards.innerHTML = `
+    <a href = "./product.html?id=${product._id}"></a>
+    <img src= "../../back-end/images/${product.imageUrl}" alt="${product.altTxt}">
+    <h3>${product.name}</h3>
+    <p>${product.description}</p>
+     `;
+  }
+}
 
-const article = document.createElement("article");
-const h3 = document.createElement("h3");
-const p = document.createElement("p");
-
-// METHOD 1 - USING appendChild
+/*
+// METHOD 1 - USING appendChild //this method is more secure but slower than using element.innterHTML();
 // insert all elements of product card in section element
-productCard.textContent = "hello hello";
+productCard.textContent = "product card";
+article.textContent = "article";
+
 section.appendChild(productCard);
 productCard.appendChild(article);
 article.append(image, h3, p);
 
-
-//*****NOTE TO MYSELF - to set up a "const href" I need to be able grab the ID number of each producct and add it to ./prodcut.html?id query - watch tut for for each loop and how to attach variables at the end of an recurring path*****
-
-/*
-METHOD 2 - USING element.innerHTML();
-
-section.innerHTML(`
-<a href="./product.html?id=a6ec5b49bd164d7fbe10f37b6363f9fb">Hello world
-</a>
-`);
-
-OR
-
-section.innerHTML = `
-<a href="${href}">
-<article>
-  <img src="${image}" alt="Photo of a pink sofa, three seats">
-  <h3 class="productName">Kanap Sinopé</h3>
-  <p class="productDescription">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "altTxt": "Photo of a blue sofa, two seats</p>
-</article>
-</a>`;
-
+productCard.setAttribute("href", "./product.html?id=${href}");
+image.setAttribute("src", "../../back-end/images/${image}");
+h3.setAttribute("class", "productName");
+p.setAttribute("class", "productDescription");
 */
