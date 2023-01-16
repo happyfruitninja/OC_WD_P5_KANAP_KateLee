f778cdf8ae9595fa3cb8b0d916259ed885507b09
happyfruitninja <happyfruitninja@gmail.com>
2023-01-16 17:04:59 -0500

    finished setting up index.html (all product cards show in the home page), created product.js, next: complete module 5 and 6 (use js to connect product info to show on product page when an item is selected)

---
+++ front-end/js/product.js
@@ -0,0 +1,3 @@
+//TODO get the product id from the url parameters (milestone 5 resource)
+//TODO use fetch API to get the product info for the product id -> copy the id number and add it to the url address at the end.  
+//TODO create a function - getElementByID etc to update. use the comments(product.html) in this function
--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,60 +1,25 @@
-//BRINING IN DATA FROM BACK-END -> this shows the objects in console
-
 fetch("http://localhost:3000/api/products/")
   .then((data) => {
     return data.json();
   })
   .then((products) => {
-     console.log(products);
+    insertProducts(products);
   });
 
-console.log("hi");
-const productCards = document.getElementById("items");
-console.log(productCards);
-
-// ACCESSING PRODUCT INFO
-//TODO: wrap all of the code below into a function called "insertProducts(products)"
+const sectionElement = document.getElementById("items");
 function insertProducts(products) {
-  //TODO: declare productCards variable and set initially to empty string const (ex.productCards = "")
-  const productCard = ""; //KATE what is this and why do I need this?
-  for (let i = 0; i < products.length; i++) {
-    const product = products[i];
-    console.log(product); //KATE console won't show populated the loop. it shows a flash but disappears
-
-    //TODO: declare a variable that is going to be a string for the commented out html in index page // KATE I don't understand this, "a string"?
-
-    const productElement = document.createElement('article');
-    
-    //  NOTE: use template string - check out the link from Scott (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals)
-    //  NOTE: add expression${} for each part of html that changes from product to product - dot notation ex. product.name =
-
-    //TODO: append the current productsCards productCards =+ productCard
-    productCards.append(productCard);
-    productCard.append(article);
-
-    //TODO: set innerHTML section element to productCards string (ex.section.innerHTML = productCards)
-
-    productCards.innerHTML = `
-    <a href = "./product.html?id=${product._id}"></a>
-    <img src= "../../back-end/images/${product.imageUrl}" alt="${product.altTxt}">
+  let productCards = ""; 
+  for (let product of products) {
+    console.log(product);
+    const productElement = document.createElement("article");
+    productCards += `
+    <a href = "./product.html?id=${product._id}">
+    <article>
+    <img src= "${product.imageUrl}" alt="${product.altTxt}">
     <h3>${product.name}</h3>
     <p>${product.description}</p>
-     `;
+    </article>
+    </a>`;
   }
+  sectionElement.innerHTML = productCards;
 }
-
-/*
-// METHOD 1 - USING appendChild //this method is more secure but slower than using element.innterHTML();
-// insert all elements of product card in section element
-productCard.textContent = "product card";
-article.textContent = "article";
-
-section.appendChild(productCard);
-productCard.appendChild(article);
-article.append(image, h3, p);
-
-productCard.setAttribute("href", "./product.html?id=${href}");
-image.setAttribute("src", "../../back-end/images/${image}");
-h3.setAttribute("class", "productName");
-p.setAttribute("class", "productDescription");
-*/
