aa35cda3aaf838cb5271a9308605a9ad28a11ff0
happyfruitninja <happyfruitninja@gmail.com>
2023-02-01 23:22:30 -0500

    Working on Module 6 - captured product Id. Next is to connect html to product details.

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -1,32 +1,30 @@
-//TODO get the product id from the url parameters (milestone 5 resource)
-//TODO use fetch API to get the product info for the product id -> copy the id number and add it to the url address at the end.  
 //TODO create a function - getElementByID etc to update. use the comments(product.html) in this function
 
-const url = "http://localhost:5500/front-end/html/product.html/product.html";
-let paramId = "";
-let queryString = new URLSearchParams(paramsId);
-
-for (let param of searchParams) {
-    console.log(paramId);
-}
+const queryString = location.search;
+const urlParams = new URLSearchParams(queryString);
+const productId = urlParams.get("id");
 
 //FIXME - fetch element?
-fetch("http://localhost:3000/api/products/")
+fetch(`http://localhost:3000/api/products/${productId}`)
   .then((data) => {
     return data.json();
   })
-  .then((paramId) => {
-    insertProducts(paramId);
+  .then((product) => {
+    insertProduct(product);
   });
 
-  url${paramId}
+function insertProduct(product) {
+  const productImage = document.querySelector(".item__img");
+  productImage.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+}
 
-
-  TODO - connect html to the product details
-  const productImage= document.getElementByClassName("item_image");
+//TODO - connect html to the product details
+/*
+  const productImage = document.querySelector(".item__img");
+ 
   productImage.innerHTML = `<img src="${product.imageURL}" alt="${product.altTxt}">`
 
-  const productTitlePrice = document.getElementByClassName("item__content__titlePrice");
+  const productTitlePrice = document.getElementsByClassName("item__content__titlePrice");
   for(element of elements) {
     let productTitlePrice = "";
     productTitlePrice += `
@@ -37,4 +35,6 @@ fetch("http://localhost:3000/api/products/")
   
 
   const productDescription = document.getElementByID("description");
+  
   productDescription.innerHTML = '<p>${product.description}</p>';
+  */
