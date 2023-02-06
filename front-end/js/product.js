f24758a58e580fd3db6443f7032eeebbc883aaa4
happyfruitnijna <happyfruitninja@gmail.com>
2023-02-06 00:16:45 -0500

    product page - product info shows for the selected product

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -15,20 +15,25 @@ function insertProduct(product) {
   //  image
   const productImage = document.querySelector(".item__img");
   productImage.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+
   //  product name and price
-  const productTitlePrice = document.querySelector("item__content__titlePrice");
-  let productTitle = "";
-  let productPrice = "";
+  let productTitle = document.getElementById("title");
+  productTitle.textContent = product.name;
+  let productPrice = document.getElementById("price");
+  productPrice.textContent = product.price;
 
   //  product description
   const productDescription = document.getElementById("description");
-  productDescription.textContent = "${product.description}";
+  productDescription.textContent = product.description;
 
   //  product color options
   const productColor = document.getElementById("colors");
-  let colorOptions = [];
-  for (let color of colors) {
+  let optionColors = "";
+  for (let color of product.colors) {
     console.log(color);
+    optionColors += `
+    <option value="${color}">${color}</option>
+    `;
+    colors.innerHTML = optionColors;
   }
-  colorOptions.innerHTML += `<option value="${product.colors[color]}">${product.colors[color]}</option>`;
 }
