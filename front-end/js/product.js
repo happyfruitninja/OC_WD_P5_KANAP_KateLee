59267f6278022899d64a4988d95da87e7ddcfa0c
happyfruitnijna <happyfruitninja@gmail.com>
2023-02-05 22:03:53 -0500

    working mildestone 6_grabbing info of selected product

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -1,10 +1,7 @@
-//TODO create a function - getElementByID etc to update. use the comments(product.html) in this function
-
 const queryString = location.search;
 const urlParams = new URLSearchParams(queryString);
 const productId = urlParams.get("id");
 
-//FIXME - fetch element?
 fetch(`http://localhost:3000/api/products/${productId}`)
   .then((data) => {
     return data.json();
@@ -13,34 +10,25 @@ fetch(`http://localhost:3000/api/products/${productId}`)
     insertProduct(product);
   });
 
+//TODO - connect html to the product details
 function insertProduct(product) {
+  //  image
   const productImage = document.querySelector(".item__img");
   productImage.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+  //  product name and price
   const productTitlePrice = document.querySelector("item__content__titlePrice");
-  let titlePriceInfo = '';
-  for(let titlePriceInfo of product) {
-    titlePrice
+  let productTitle = "";
+  let productPrice = "";
+
+  //  product description
+  const productDescription = document.getElementById("description");
+  productDescription.textContent = "${product.description}";
+
+  //  product color options
+  const productColor = document.getElementById("colors");
+  let colorOptions = [];
+  for (let color of colors) {
+    console.log(color);
   }
-  titlePriceInfo.innerHTML = ``
+  colorOptions.innerHTML += `<option value="${product.colors[color]}">${product.colors[color]}</option>`;
 }
-
-//TODO - connect html to the product details
-/*
-  
-  
-  
-  
-  
-  for(element of elements) {
-    let productTitlePrice = "";
-    productTitlePrice += `
-    <h1>${product.name}</h1>
-    <p>Prix: ${product.price}</p>
-    `
-  }
-  
-
-  const productDescription = document.getElementByID("description");
-  
-  productDescription.innerHTML = '<p>${product.description}</p>';
-  */
