b21aee3525366d3fbd36d83ab99dd34c8d9f87ef
happyfruitninja <happyfruitninja@gmail.com>
2023-02-26 15:26:44 -0500

    working on setting up insertCart function for milestone

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -1,2 +1,22 @@
 //TODO insert cartItem cards into the page using loop (through the cart array). keep in mind each item has productId and I need to fetch to grab product details such as description and name (use fetch API from backend) Similar to milestone 3
 //NOTE total quantity and total price (TIP: as you are iterating through -> update the running totals on the page not on localstorage/backend)
+
+const queryString = location.search;
+const urlParams = new URLSearchParams(queryString);
+const productId = urlParams.get("id");
+
+fetch(`http://localhost:3000/api/products/${productId}`)
+  .then((data) => {
+    return data.json();
+  })
+  .then((cartItem) => {
+    insertProduct(cartItem);
+  });
+
+//connect html to the items in cart
+function insertCart(cartItem) {
+  //    image
+  //    description
+  //    product name, color and price  
+  //    product quantity
+}
