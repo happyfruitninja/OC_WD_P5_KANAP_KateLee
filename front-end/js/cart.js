7603412ce6b5ed0ec44fbfd4401bce9d57e90f70
Lee <jihyeule@amazon.com>
2023-03-06 17:50:32 -0500

    milestone 8 : finished grabbing cartitem info. Next: work on updating the totalNumber and totalPrice

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -1,7 +1,6 @@
 //TODO insert cartItem cards into the page using loop (through the cart array). keep in mind each item has productId and I need to fetch to grab product details such as description and name (use fetch API from backend) Similar to milestone 3
 //NOTE total quantity and total price (TIP: as you are iterating through -> update the running totals on the page not on localstorage/backend)
 
-
 fetch("http://localhost:3000/api/products/")
   .then((data) => {
     return data.json();
@@ -20,37 +19,54 @@ function insertCart(products) {
   console.log(cart);
   for (let cartItem of cart) {
     //TODO get the product from product's array for current cartItem.id
-    //   const found = cart.find((element) => {
-    //  return element.color === selectedColor && element.productId productId;
-  // }); 
+    const found = products.find((product) => {
+      return product._id === cartItem.productId;
+    });
+    console.log(found);
+    //same as const found = products.find(product => product.productId === cartItem.productId); used where there is one line of code
+
     const cartArticle = document.createElement("article");
     cartArticle.classList.add("cart__item");
-     //TODO enter expression using ${}
-    cartItemCards += `
-    <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+    cartArticle.dataset.id = found._id;
+    cartArticle.dataset.color = cartItem.color;
+
+    cartArticle.innerHTML = `
             <div class="cart__item__img">
-              <img src="../images/product01.jpg" alt="Photo of a sofa">
+              <img src="${found.imageUrl}" alt="${found.altTxt}">
             </div>
             <div class="cart__item__content">
               <div class="cart__item__content__description">
-                <h2>Name of the product</h2>
-                <p>Green</p>
-                <p>€42.00</p>
+                <h2>${found.name}</h2>
+                <p>${cartItem.color}</p>
+                <p>€${found.price}</p>
               </div>
               <div class="cart__item__content__settings">
                 <div class="cart__item__content__settings__quantity">
                   <p>Qté : </p>
-                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cartItem.quantity}">
                 </div>
                 <div class="cart__item__content__settings__delete">
                   <p class="deleteItem">Delete</p>
                 </div>
               </div>
-            </div>
-          </article>
+            </div>        
     `;
-    
+
     //TODO update total price and total quantity for current cartItem - we need a number to do arithmatic calculation ie) const selectedQuantity = parseInt(quantityElement.value);
+
+    //TODOget the current total off the page + partInt the number string
+    //  update the total number on the page with current cartItem quantity
+    //  use innerText to inject the updated total number  between the <span></span> + artItem.quantity * found.price
+    //  repeat process for price
+    // const selectedQuantity = parseInt(.value);
+    // const selectedPrice = parseInt(priceElement.value);
+    // const totalQuantity = 0;
+    // const totalPrice = 0;
+
+    sectionCartItem.appendChild(cartArticle);
   }
-  sectionCartItem.innerHTML = cartItemCards;
 }
+
+//TODOadd eventListener
+//parseInt -
+// .innerText
