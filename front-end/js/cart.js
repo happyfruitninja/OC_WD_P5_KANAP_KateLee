29c2dca9d455ffd1a16eb493a8f443b2de046772
Lee <jihyeule@amazon.com>
2023-03-20 00:07:23 -0400

    milestone 9 : working on updating quantity and deleting items from cart

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -1,4 +1,4 @@
-//TODO insert cartItem cards into the page using loop (through the cart array). keep in mind each item has productId and I need to fetch to grab product details such as description and name (use fetch API from backend) Similar to milestone 3
+//insert cartItem cards into the page using loop (through the cart array). keep in mind each item has productId and I need to fetch to grab product details such as description and name (use fetch API from backend) Similar to milestone 3
 //NOTE total quantity and total price (TIP: as you are iterating through -> update the running totals on the page not on localstorage/backend)
 
 fetch("http://localhost:3000/api/products/")
@@ -17,7 +17,7 @@ function insertCart(products) {
   let cart = JSON.parse(window.localStorage.getItem("cart")) || [];
   console.log(cart);
   for (let cartItem of cart) {
-    //TODO get the product from product's array for current cartItem.id
+    //get the product from product's array for current cartItem.id
     const found = products.find((product) => {
       return product._id === cartItem.productId;
     });
@@ -27,8 +27,6 @@ function insertCart(products) {
     cartArticle.classList.add("cart__item");
     cartArticle.dataset.id = found._id;
     cartArticle.dataset.color = cartItem.color;
-
-    //same as const ie) found = products.find(product => product.productId === cartItem.productId); used where there is one line of code
 
     cartArticle.innerHTML = `
             <div class="cart__item__img">
@@ -51,38 +49,50 @@ function insertCart(products) {
               </div>
             </div>        
     `;
+    //TODO add a change eventListener to the input field for quantity
+    //   note : to use querySelector -> cartArticle.querySelector(".itemQuantity")
 
+    let itemQuantity = cartArticle.querySelector(".itemQuantity");
+
+    itemQuantity.addEventListener("change", () => {
+      let updatedQuantity = "";
+      updatedQuantity = updatedQuantity + itemQuantity.value;
+      console.log(updatedQuantity);
+    });
+
+    //----------------------------------------------
+
+    //TODO add a click eventListener to delete <p> tag
+    //   note : similar process as above
+    const deleteItem = cartArticle.querySelector(".deleteItem");
+    deleteItem.addEventListener("click", () => {
+     cartArticle.remove();
+    });
+
+    //---------------------------------------------
     sectionCartItem.appendChild(cartArticle);
 
-    // TODO update total price and total quantity for current cartItem - we need a number to do arithmatic calculation ie) const selectedQuantity = parseInt(quantityElement.value);
+    //update total price and total quantity for current cartItem - we need a number to do arithmatic calculation ie) const selectedQuantity = parseInt(quantityElement.value);
 
     //  convert string quantity in cartItem into number
-    //TODOget the current total off the page + partInt the number string
+    // get the current total off the page + partInt the number string
     //  update the total number on the page with current cartItem quantity
     //get span element with #totalQuantity
 
     let currentQuantity =
-      parseInt(document.getElementById("totalQuantity").innerText) || "";
-
-    currentQuantity = currentQuantity + cartItem.quantity;
+      parseInt(document.getElementById("totalQuantity").innerText) || 0;
+    currentQuantity += cartItem.quantity;
 
     totalQuantity.innerText = currentQuantity;
     console.log(totalQuantity);
 
-    //  use innerText to inject the updated total number between the <span></span> + cartItem.quantity * found.price
-    //  repeat process for price
-
-    let price = parseInt(document.getElementById("totalPrice").innerText) || "";
-    price += found.price;
-
-    const currentPrice = currentQuantity * price;
+    let currentPrice =
+      parseInt(document.getElementById("totalPrice").innerText) || 0;
+    currentPrice += found.price * cartItem.quantity;
     totalPrice.innerText = currentPrice;
-    //FIXME TODO currently it's adding the ([0] * totalQuantity * found.price) + ([1] * totalQuantity * found.price)
-    // TODO for loop ? to go through each items and get total price for each item and then totalPrice needs to show the total of all totalPrice
-    console.log(totalPrice);
 
-    //TODOadd eventListener
-    //parseInt -
-    // .innerText
+    console.log(totalPrice);
   }
 }
+
+//TODO new function : this will change the handle to change eventListener. Same is required for delete
